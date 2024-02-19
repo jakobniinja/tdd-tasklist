c80efc7f0952d839c885da4000382ccdcc9afbf5
jakobniinja <dansaren124@outlook.com>
2024-02-19 11:11:45 +0100

    Convert class to record

--- src/main/java/com/jakobniinja/tasks/Task.java
+++ src/main/java/com/jakobniinja/tasks/Task.java
@@ -1,22 +1,5 @@
 package com.jakobniinja.tasks;
 
-public class Task {
+public record Task(long id, String description, boolean done) {
 
-  private long id = 0L;
-
-  private final String description;
-
-
-  public Task(long id, String description, boolean done) {
-    this.id = id;
-    this.description = description;
-  }
-
-  public long getId() {
-    return id;
-  }
-
-  public String getDescription() {
-    return description;
-  }
 }
--- src/test/java/com/jakobniinja/tasks/TaskTest.java
+++ src/test/java/com/jakobniinja/tasks/TaskTest.java
@@ -1,7 +1,9 @@
 package com.jakobniinja.tasks;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
@@ -18,11 +20,22 @@ class TaskTest {
   void onTaskGetId() {
 
     Task task = new Task(0, "Read book for 20 minutes", false);
-    assertEquals(0L, task.getId());
+    assertEquals(0L, task.id());
   }
 
   @Test
   void getDescAfterConstruction() {
-    assertEquals("Read book for 20 minutes", task.getDescription());
+    assertEquals("Read book for 20 minutes", task.description());
+  }
+
+  @Test
+  void onTaskInitNotDone(){
+    assertFalse(task.done());
+  }
+
+  @Test
+  void onTaskDone(){
+    task = new Task(0, "Done task", true);
+    assertTrue(task.done());
   }
 }
