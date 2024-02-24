952b21f3ae44827ffea3c3773252eaf460a4809e
jakobniinja <dansaren124@outlook.com>
2024-02-24 07:45:19 +0100

    Introduce global constants

--- src/test/java/com/jakobniinja/tasks/TaskListTest.java
+++ src/test/java/com/jakobniinja/tasks/TaskListTest.java
@@ -12,6 +12,18 @@ import org.junit.jupiter.api.Test;
 
 class TaskListTest {
 
+  private static final String EXPECTED0 = "Best Tdd project so far! write first unit test. progress: 0";
+
+  private static final String EXPECTED = "Best Tdd project so far! write first unit test. progress: X";
+
+  private static final String BEST_TDD_PROJECT_SO_FAR = "Best Tdd project so far!";
+
+  private static final String WRITE_FIRST_UNIT_TEST = "write first unit test.";
+
+  private static final String STRING_ID = "0";
+
+  private static final String STRING_ID1 = "1";
+
   private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
 
   TaskList taskList = new TaskList();
@@ -36,31 +48,29 @@ class TaskListTest {
 
   @Test
   void onShowHasItem() {
-    taskList.addProject("Best Tdd project so far!");
-    taskList.addTask("Best Tdd project so far!", "write first unit test.");
-    taskList.setDone("0", true);
+    taskList.addProject(BEST_TDD_PROJECT_SO_FAR);
+    taskList.addTask(BEST_TDD_PROJECT_SO_FAR, WRITE_FIRST_UNIT_TEST);
+    taskList.setDone(STRING_ID, true);
 
     taskList.show();
 
-    String expected = "Best Tdd project so far! write first unit test. progress: X";
-    assertEquals(expected, outContent.toString());
+    assertEquals(EXPECTED, outContent.toString());
   }
 
   @Test
   void onShowHasItemUndone() {
-    taskList.addProject("Best Tdd project so far!");
-    taskList.addTask("Best Tdd project so far!", "write first unit test.");
-    taskList.setDone("0", false);
+    taskList.addProject(BEST_TDD_PROJECT_SO_FAR);
+    taskList.addTask(BEST_TDD_PROJECT_SO_FAR, WRITE_FIRST_UNIT_TEST);
+    taskList.setDone(STRING_ID, false);
 
     taskList.show();
 
-    String expected = "Best Tdd project so far! write first unit test. progress: 0";
-    assertEquals(expected, outContent.toString());
+    assertEquals(EXPECTED0, outContent.toString());
   }
 
   @Test
   void onAddProjectNotEmpty() {
-    taskList.addProject("New tdd-java project");
+    taskList.addProject(BEST_TDD_PROJECT_SO_FAR );
 
     assertFalse(taskList.tasks.keySet().isEmpty());
   }
@@ -68,69 +78,58 @@ class TaskListTest {
 
   @Test
   void onAddTaskNull() {
-    taskList.addTask(null, "my first testing task");
+    taskList.addTask(null, WRITE_FIRST_UNIT_TEST);
     assertTrue(taskList.getTasks().isEmpty());
   }
 
   @Test
   void onAddTaskValid() {
-    taskList.addProject("New tdd-java project");
-    taskList.addTask("New tdd-java project", "my first testing task");
+    taskList.addProject(BEST_TDD_PROJECT_SO_FAR );
+    taskList.addTask(BEST_TDD_PROJECT_SO_FAR , WRITE_FIRST_UNIT_TEST);
     assertEquals(1, taskList.getTasks().size());
   }
 
   @Test
   void onMatchDoneTrue() {
-    taskList.addProject("New tdd-java project");
-    taskList.addTask("New tdd-java project", "my first testing task");
-    taskList.setDone("0", true);
+    taskList.addProject(BEST_TDD_PROJECT_SO_FAR);
+    taskList.addTask(BEST_TDD_PROJECT_SO_FAR, WRITE_FIRST_UNIT_TEST);
+    taskList.setDone(STRING_ID, true);
 
     assertTrue(taskList.getTasks().stream().anyMatch(Task::isDone));
   }
 
   @Test
   void onMissDoneFalse() {
-    taskList.addProject("New tdd-java project");
-    taskList.addTask("New tdd-java project", "my first testing task");
-    taskList.setDone("1", true);
+    taskList.addProject(BEST_TDD_PROJECT_SO_FAR);
+    taskList.addTask(BEST_TDD_PROJECT_SO_FAR, WRITE_FIRST_UNIT_TEST);
+    taskList.setDone(STRING_ID1, true);
 
     assertFalse(taskList.getTasks().stream().anyMatch(Task::isDone));
   }
 
   @Test
   void onCheckReturnTrue() {
-    taskList.addProject("New tdd-java project");
-    taskList.addTask("New tdd-java project", "my first testing task");
-    taskList.check("1", true);
+    taskList.addProject(BEST_TDD_PROJECT_SO_FAR);
+    taskList.addTask(BEST_TDD_PROJECT_SO_FAR, WRITE_FIRST_UNIT_TEST);
+    taskList.check(STRING_ID1, true);
 
     assertFalse(taskList.getTasks().stream().anyMatch(Task::isDone));
   }
 
   @Test
   void onInitUnsetDone() {
-    taskList.addProject("New tdd-java project");
-    taskList.addTask("New tdd-java project", "my first testing task");
-    taskList.setDone("1", false);
+    taskList.addProject(BEST_TDD_PROJECT_SO_FAR);
+    taskList.addTask(BEST_TDD_PROJECT_SO_FAR, WRITE_FIRST_UNIT_TEST);
+    taskList.setDone(STRING_ID1, false);
 
     assertTrue(taskList.getTasks().stream().noneMatch(Task::isDone));
   }
-
 
   @Test
   void onUncheckFalse() {
-    taskList.addProject("New tdd-java project");
-    taskList.addTask("New tdd-java project", "my first testing task");
-    taskList.unCheck("1", false);
-
-    assertTrue(taskList.getTasks().stream().noneMatch(Task::isDone));
-  }
-
-  @Test
-  void onPreviouslyFalse() {
-    taskList.addProject("New tdd-java project");
-    taskList.addTask("New tdd-java project", "my first testing task");
-    taskList.setDone("1", true);
-    taskList.setDone("1", false);
+    taskList.addProject(BEST_TDD_PROJECT_SO_FAR);
+    taskList.addTask(BEST_TDD_PROJECT_SO_FAR, WRITE_FIRST_UNIT_TEST);
+    taskList.unCheck(STRING_ID1, false);
 
     assertTrue(taskList.getTasks().stream().noneMatch(Task::isDone));
   }
